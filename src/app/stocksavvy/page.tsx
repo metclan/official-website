e07127105e39b81571e9a794e9121a2fda97bee3
Harmony <bluedonice@gmail.com>
2023-11-26 12:19:56 +0100

    replaced ' with &ampos;

--- src/app/stocksavvy/page.tsx
+++ src/app/stocksavvy/page.tsx
@@ -38,12 +38,12 @@ export default function Stocksavvy() {
           Stocksavvy is a user-friendly software designed to make life easier
           for business owners like you. With Stocksavvy, you can effortlessly
           monitor your inventory, sales, and even keep an eye on products that
-          are close to their expiration date. It's not just about tracking; this
-          tool also does the math for you, helping you figure out your profits
-          and losses. Running low on a popular product? Stocksavvy gives you a
-          heads-up so you can restock before it's too late. And if you're
-          managing multiple locations, no worries – Stocksavvy is your go-to
-          solution for seamless tracking across all your spots.
+          are close to their expiration date. It&apos;s not just about tracking;
+          this tool also does the math for you, helping you figure out your
+          profits and losses. Running low on a popular product? Stocksavvy gives
+          you a heads-up so you can restock before it&apos;s too late. And if
+          you&apos;re managing multiple locations, no worries – Stocksavvy is
+          your go-to solution for seamless tracking across all your spots.
         </p>
       </div>
       <Image
@@ -66,12 +66,12 @@ export default function Stocksavvy() {
           <p className="text-sm mt-2 leading-relaxed">
             If you own a store, Stocksavvy is like having a superhero friend to
             help you. Stocksavvy tells you if your store is not making enough
-            money. It's like a special detective friend that helps you find out
-            why this is happening. Maybe you're buying too many of one thing, or
-            someone who works with you is doing something wrong. Stocksavvy,
-            your special friend, also tells you how much all your things in the
-            store are worth. It's like having a magic helper, Stocksavvy, that
-            makes sure your store is doing great!
+            money. It&apos;s like a special detective friend that helps you find
+            out why this is happening. Maybe you&apos;re buying too many of one
+            thing, or someone who works with you is doing something wrong.
+            Stocksavvy, your special friend, also tells you how much all your
+            things in the store are worth. It&apos;s like having a magic helper,
+            Stocksavvy, that makes sure your store is doing great!
           </p>
         </div>
         <Image
@@ -95,13 +95,13 @@ export default function Stocksavvy() {
             Detailed Stock Report
           </h3>
           <p className="text-sm mt-2 leading-relaxed">
-            As the boss of your business, it's super important to know all about
-            your stock report. This report tells you how much money you used to
-            buy all your stuff, how much money you could get by selling
+            As the boss of your business, it&apos;s super important to know all
+            about your stock report. This report tells you how much money you
+            used to buy all your stuff, how much money you could get by selling
             everything, and how much extra money you might make. Your profit
             margin on your individual products will be shown to you. Guess what?
             With Stocksavvy, you can find all this out by just clicking a
-            button! It's like magic for your business!
+            button! It&apos;s like magic for your business!
           </p>
         </div>
       </div>
@@ -115,7 +115,7 @@ export default function Stocksavvy() {
             everything about the products you sell! You can know what was sold,
             when it was sold, how much of it was sold, who bought it, who sold
             it, how much it was sold for, and even how the customer paid for it.
-            It's like having a superpower for your business, making you the
+            It&apos;s like having a superpower for your business, making you the
             superhero boss!
           </p>
         </div>
@@ -133,14 +133,15 @@ export default function Stocksavvy() {
             End of day reports
           </h3>
           <p className="text-sm mt-2 leading-relaxed">
-            Stocksavvy is like a super assistant for you and your team when it's
-            time to wrap up the day. Instead of spending a lot of time with pen
-            and paper, Stocksavvy gives you a special report. This report tells
-            you everything you need to know, like how much stuff you sold for
-            the day, how much money you got in cash, through the bank, pos and
-            from card payments. It even adds up all your sales to give you a big
-            total! This saves a ton of time and makes sure everything adds up
-            correctly at the end of the day. Super handy, right?
+            Stocksavvy is like a super assistant for you and your team when
+            it&apos;s time to wrap up the day. Instead of spending a lot of time
+            with pen and paper, Stocksavvy gives you a special report. This
+            report tells you everything you need to know, like how much stuff
+            you sold for the day, how much money you got in cash, through the
+            bank, pos and from card payments. It even adds up all your sales to
+            give you a big total! This saves a ton of time and makes sure
+            everything adds up correctly at the end of the day. Super handy,
+            right?
           </p>
         </div>
         <Image

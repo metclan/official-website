c2467533800cb566fa779e083dbbca17a65d23cf
mac <mac@Users-MacBook-Pro.local>
2024-09-13 23:25:09 +0100

    removed jobs list link

--- src/app/(authenticated)/user/jobs/page.tsx
+++ src/app/(authenticated)/user/jobs/page.tsx
@@ -1,18 +1,12 @@
 import { Button } from "@/components/ui/button"
 import { PlusIcon } from "lucide-react"
 import Link from "next/link"
-import RenderJobCards from "./components/renderjobcards"
 export const metadata = {
   title: "Job Listings",
   description: "View all your job listings"
 }
 
 export default async function JobListingPage() {
-  const fetchJobs = await fetch("http://localhost:3000/user/jobs/api");
-  const jobs = await fetchJobs.json();
-  if (!jobs) {
-    return <div>No jobs found</div>
-  }
   return (
     <div className="p-6 max-w-6xl mx-auto w-full">
       <div className="flex justify-between items-center mb-6">
@@ -24,7 +18,7 @@ export default async function JobListingPage() {
         </Button>
 
       </div>
-      <RenderJobCards jobs={jobs} />
+      {/* <RenderJobCards jobs={jobs} /> */}
     </div>
   )
 }
